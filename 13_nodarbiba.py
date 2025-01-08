952c2e68ccc4595cc8488429040ec51f833f1749
Ingmars Melkis <idmelkis@edu.riga.lv>
2025-01-08 09:11:12 +0200

    08.01 stundas kods 2.daļa

--- 13_nodarbiba.py
+++ 13_nodarbiba.py
@@ -33,24 +33,66 @@ class Automasina:
         if self.atrums > 0:
             self.atrums -= 0.5
 
-objekts = Automasina("Sarkans", "BMW")
-objekts2 = Automasina("Zils", "Audi")
-print(objekts.getAtrums())
-print(objekts)
-print(objekts.__dict__)
+# objekts = Automasina("Sarkans", "BMW")
+# objekts2 = Automasina("Zils", "Audi")
+# print(objekts.getAtrums())
+# print(objekts)
+# print(objekts.__dict__)
 #Automasina.klasesmetode()
 
 # Programma - simulācija kurā automašīnu var braukt (paātrināt un bremzēt)
-nobraukts = 0.0
-masina = Automasina("kaut kada", "masina")
-while True:
-    i = input() # Problēma: bloķē ciklu, idejiski vajadzētu nebloķējošu ievadi.
-    if i == "w":
-        masina.paatrinat()
-    elif i == "s":
-        masina.bremzet()
+# nobraukts = 0.0
+# masina = Automasina("kaut kada", "masina")
+# while True:
+#     i = input() # Problēma: bloķē ciklu, idejiski vajadzētu nebloķējošu ievadi.
+#     if i == "w":
+#         masina.paatrinat()
+#     elif i == "s":
+#         masina.bremzet()
 
-    nobraukts += masina.getAtrums()
-    print(f"masina {masina.krasa} {masina.marka} ir nobraukusi {nobraukts},\
-paslaik brauc ar atrumu {masina.getAtrums()}")
+#     nobraukts += masina.getAtrums()
+#     print(f"masina {masina.krasa} {masina.marka} ir nobraukusi {nobraukts},\
+# paslaik brauc ar atrumu {masina.getAtrums()}")
 
+# Uzdevums: 
+# Izveidot klasi, kas aprakstītu priekšmetu e-klasē
+# Vismaz 4 mainīgie, un 2 funkcijas kas strādā ar šiem datiem.
+# Izveidojat objektu, un uzrakstat kodu, kas pielieto šīs funkcijas.
+class Skolens:
+    vards = ""
+    def __init__(self, vards, uzvards):
+        self.vards += vards + " " + uzvards
+class Nodarbiba:
+    datums :str = ""
+    tema :str = ""
+    def __init__(self, datums, tema):
+        self.datums = datums
+        self.tema = tema
+class Prieksmets:
+    nosaukums :str = ""
+    skolotajs :str = ""
+    skoleni :'list[Skolens]' = []
+    nodarbibas :'list[Nodarbiba]' = []
+    def __init__(self, nosaukums, skolotajs):
+        self.nosaukums = nosaukums
+        self.skolotajs = skolotajs
+    def __str__(self):
+        return f"Priekšmets {self.nosaukums} ar {len(self.skoleni)} skolēniem."
+    
+    def izveidotNodarbibu(self, datums :str, tema :str):
+        self.nodarbibas.append(Nodarbiba(datums, tema))
+    def pievienotSkolenu(self, skolens :Skolens):
+        self.skoleni.append(skolens)
+
+programmesana = Prieksmets("Programmēšana I", "Ingmārs")
+programmesana.pievienotSkolenu(Skolens("Jānis", "Bērziņš"))
+print(programmesana)
+
+fizika = Prieksmets("Fizika", "Mihno")
+fizika.pievienotSkolenu(Skolens("Jānis", "Bērziņš"))
+print(fizika)
+
+# Uzdevums 2: 
+# Uzrakstīt klasi "Lampa", kas apraksta lampu, un spuldzi tajā.
+# Lampas objektam vajadzētu uzglabāt informāciju par lampas stāvokli,
+# un spuldzi tajā.
